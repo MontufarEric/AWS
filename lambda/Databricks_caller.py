f049842eae9091c2c2e030e0363f243b4ae4cfdc
emont <eric.montufar@gmail.com>
2021-03-31 19:06:11 -0500

    add sqd with DLQ

--- lambda/Databricks_caller.py
+++ lambda/Databricks_caller.py
@@ -8,3 +8,5 @@ response = requests.post(
 )
 if response.status_code == 200: print(response.json())
 else: print("Error launching cluster: %s" % (response.json()))
+
+#https://databricks.com/blog/2016/10/11/using-aws-lambda-with-databricks-for-etl-automation-and-ml-model-serving.html?_ga=2.34112865.1767610906.1617235120-1531116094.1601927735
