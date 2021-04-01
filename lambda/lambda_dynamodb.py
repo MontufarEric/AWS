a82e9aac87346044aecc594188da62f7f6fd86a9
emont <eric.montufar@gmail.com>
2021-03-31 19:56:23 -0500

    add lambda dynamo

---
+++ lambda/lambda_dynamodb.py
@@ -0,0 +1,22 @@
+def lambda_handler(event, context):
+    print('Incoming event: ', event)
+    print('Incoming state: ', event['state'])
+
+#Check if this is the end of the window to either aggregate or process.
+    if event['isFinalInvokeForWindow']:
+        # logic to handle final state of the window
+        print('Destination invoke')
+    else:
+        print('Aggregate invoke')
+
+#Check for early terminations
+    if event['isWindowTerminatedEarly']:
+        print('Window terminated early')
+
+    #Aggregation logic
+    state = event['state']
+    for record in event['Records']:
+        state[record['dynamodb']['NewImage']['Id']] = state.get(record['dynamodb']['NewImage']['Id'], 0) + 1
+
+    print('Returning state: ', state)
+    return {'state': state}
