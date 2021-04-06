b30f458799c9dafb529bc8c7ad4d050fa185fde8
emont <eric.montufar@gmail.com>
2021-04-06 08:39:01 -0500

    add lambda athena

--- lambda/lambda_athena_caller.py
+++ lambda/lambda_athena_caller.py
@@ -5,3 +5,21 @@ query = "SELECT *  FROM myDatabase.MyTable"
 DATABASE = 'MyDatabase'
 Table = "MyTable"
 output='s3://my-s3-bucket'
+
+
+def lambda_handler(event, context):
+    query = "SELECT *  FROM myDatabase.MyTable"
+    client = boto3.client('athena')
+
+    # Execution
+    response = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': DATABASE
+        },
+        ResultConfiguration={
+            'OutputLocation': output,
+        }
+    )
+    return response
+    return
