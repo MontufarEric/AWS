f9b3022b9d1c7adc3353a17defd4990cc447e04c
emont <eric.montufar@gmail.com>
2021-04-13 17:09:18 -0500

    add boto3 sqs write

--- boto3/SQS_writer.py
+++ boto3/SQS_writer.py
@@ -1,3 +1,5 @@
+import boto3
+import uuid
 
 session = boto3.Session(      aws_access_key_id="your-access-key",
 							  aws_secret_access_key="your-secret-access-key"
@@ -7,7 +9,7 @@ session = boto3.Session(      aws_access_key_id="your-access-key",
 sqs = session.resource("sqs")
 
 queue2 = sqs.get_queue_by_name(QueueName="test_mdm_gateway_inbound_retry")
-response = queue2.send_message(MessageBody='hello world from boto' )
+response = queue2.send_message(MessageBody='hello world from boto' + str(uuid.uuid1()))
 
 
 # The response is NOT a resource, but gives you a message ID and MD5
